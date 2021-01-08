3426cd3d2605744e26522e65f6995f70d4ed79b8
Michael Schaufelberger <michael.schufi@gmail.com>
2021-01-08 13:50:56 +0100

    Merge branch 'master' into 'parallelization'

--- _download_parallel.mjs
+++ _download_parallel.mjs
@@ -1,17 +1,21 @@
 import { spawn, spawnSync } from 'child_process';
 import { readFileSync } from 'fs';
+import path from 'path';
 
-const spawnDestreamer = (url, sync = false) => {
+const spawnDestreamer = (url, outputDir, sync = false) => {
   const args = [
     'node',
     [
       '--max-http-header-size',
       '32768',
       'build/src/destreamer.js',
+      '-k',
       '-u',
       'youremail@example.com',
       '-i',
       url,
+      '-o',
+      outputDir,
       '-x',
       sync ? '-s' : '',
     ],
@@ -26,6 +30,8 @@ const spawnDestreamer = (url, sync = false) => {
 (async () => {
   const videos = readFileSync(process.argv[2]).toString().split('\n');
 
+  const outputDir = path.parse(process.argv[2]).dir
+
   const videoCollections = videos.reduce((acc, current, index) => {
     if (index % 10 === 0) {
       acc.push([current]);
@@ -39,12 +45,12 @@ const spawnDestreamer = (url, sync = false) => {
   console.log(videoCollections);
 
   for (const videos of videoCollections) {
-    const ret = spawnDestreamer(videos[0], true);
+    const ret = spawnDestreamer(videos[0], '.', true);
     if (ret.status != 0) {
       process.exit(1);
     }
     await Promise.all(videos.map((url) => {
-      const childProcess = spawnDestreamer(url);
+      const childProcess = spawnDestreamer(url, outputDir);
 
       return new Promise(function (resolve, reject) {
         childProcess.addListener('error', reject);
